753de3f11d2cb6e9bdbd289c3401f31b3e954638
Fernando Valverde <fernando@fdo.cr>
2022-12-27 20:15:58 -0600

    Small fix

--- src/strategy/chase_random_food.cr
+++ src/strategy/chase_random_food.cr
@@ -1,6 +1,9 @@
 # Strategy that chases randomly selected food from the board
 class Strategy::ChaseRandomFood < Strategy::Base
   def move
+    # Use RandomValid if no food is available
+    return Strategy::RandomValid.new(@context).move if @context.board.food.empty?
+
     # Chase a random food
     res = Utils.a_star(@context.you.head, @context.board.food.sample, @context)
 
