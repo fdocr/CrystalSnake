b4494d1cc309e4b4b7d7cad1ebb2ecfdbbe96058
Fernando Valverde <fernando@fdo.cr>
2023-02-07 16:28:44 -0600

    SafeEater

--- src/strategy/utils/flood_fill.cr
+++ src/strategy/utils/flood_fill.cr
@@ -7,23 +7,31 @@ require "priority-queue"
 # start off a Flood Fill and returns a Set(BattleSnake::Point) with all the
 # points reachable to that area on the board
 def Strategy::Utils.flood_fill(a : BattleSnake::Point, context : BattleSnake::Context)
+  # p "Flood fill"
   area = Set(BattleSnake::Point).new
-  queue = [a]
+  queue = [] of BattleSnake::Point
+  current = a
 
-  # Iterate through the reachable area
-  while queue.any?
-    # Include current Point from queue
-    current = queue.pop
-    area.add(current)
-
+  loop do
     # Check all valid moves from current Point
-    context.valid_moves(current)[:neighbors].each do |point|
-      # Skip if already in consideration
-      next if area.includes?(point) || queue.includes?(point)
+    context.valid_moves(current)[:neighbors].each_value.each do |point|
+      # p "Move #{point.inspect}"
+      # Skip if already in area or in queue to be processed
+      next unless area.index { |p| (p <=> point).zero? }.nil?
+      next unless queue.index { |p| (p <=> point).zero? }.nil?
 
+      # p "Adding to the queue #{point.inspect}"
       queue.push(point)
     end
+
+    current = queue.pop
+    # p "Checking #{current.inspect}"
+    area.add(current) unless current.nil?
+
+    break if queue.empty?
   end
+
+  # p "Flood fill res count: #{area.count { true }}"
 
   area
 end
