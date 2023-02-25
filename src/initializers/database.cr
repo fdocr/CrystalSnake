58a086f87c773e9cb123f1294771513fa58c3d73
Fernando Valverde <fernando@fdo.cr>
2023-02-25 04:57:39 -0600

    Add pool size config

--- src/initializers/database.cr
+++ src/initializers/database.cr
@@ -8,6 +8,7 @@ Jennifer::Config.configure do |conf|
   conf.from_uri(ENV["DATABASE_URI"]) if ENV.has_key?("DATABASE_URI")
   conf.logger.level = log_level
   conf.adapter = "postgres"
+  conf.pool_size = (ENV["DB_POOL"] ||= "5").to_i
 end
 
 Log.setup "db", log_level, Log::IOBackend.new(formatter: Jennifer::Adapter::DBFormatter)
