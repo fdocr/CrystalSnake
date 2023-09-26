035e4cf409f91de4adba304ba71f0fbec752a3cb
Fernando Valverde <fernando@fdo.cr>
2023-09-25 18:18:56 -0600

    Strategy docs

--- src/strategy/blast_random_valid.cr
+++ src/strategy/blast_random_valid.cr
@@ -1,4 +1,6 @@
-# A strategy that chooses a random valid move, or "up" if there isn't any
+# A strategy that chooses a random valid move, or "up" if there isn't any.
+# Instead of the plain `BattleSnake::Conext::valid_moves` this strategy
+# uses `BattleSnake::Conext::valid_moves` for a smarter choice if possible.
 class Strategy::BlastRandomValid < Strategy::Base
   def move
     valid_moves = @context.blast_valid_moves!
