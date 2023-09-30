746690338f30846513f33ac5ad7d19651194978e
Fernando Valverde <fernando@fdo.cr>
2023-09-29 18:33:29 -0600

    Fix for /games + small enhancements (#29)
    * Revert mosquito to 1.0.0.rc3 + filter /games per strategy + script helper
    
    * Embed worker within app is needed

--- src/sam.cr
+++ src/sam.cr
@@ -9,30 +9,14 @@ require "../db/migrations/*"
 load_dependencies "jennifer"
 
 task "dev" do
-  sentry = [
-    Sentry::ProcessRunner.new(
-      display_name: "App",
-      build_command: "crystal build ./src/app.cr",
-      run_command: "./app",
-      run_args: ["-p", "8080"],
-      files: [ "./src/**/*" ]
-    ),
-    Sentry::ProcessRunner.new(
-      display_name: "Worker",
-      build_command: "crystal build ./src/worker.cr",
-      run_command: "./worker",
-      files: [ "./src/jobs/*.cr" ]
-    )
-  ]
-
-  # Execute runners in separate threads
-  sentry.each { |s| spawn { s.run } }
-
-  begin
-    sleep
-  rescue
-    sentry.each(&.kill)
-  end
+  sentry = Sentry::ProcessRunner.new(
+    display_name: "App",
+    build_command: "crystal build ./src/app.cr",
+    run_command: "./app",
+    run_args: ["-p", "8080"],
+    files: [ "./src/**/*", "./config/*.cr" ]
+  )
+  sentry.run
 end
 
 task "test" do
